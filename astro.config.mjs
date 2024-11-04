9cc915b93f3e9e1fdb334e2f64aba044cd5702ab
ramicurros <ramicurros@gmail.com>
2024-11-04 13:27:01 -0300

    config site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ramicurros.github.io/escala-vgv-page/',
+	site: 'https://ramicurros.github.io',
 	base: 'escala-vgv',
 	integrations: [mdx(), sitemap()],
 });
